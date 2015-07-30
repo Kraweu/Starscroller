2d01bd9a8d7836f405a6d542c84ab374624a52be
Kraweu <kraweu@gmx.at>
2015-07-31 00:03:20 +0200

    Collison detection Border Implemented

--- core/src/at/kraweu/collisionDetection/Border.java
+++ core/src/at/kraweu/collisionDetection/Border.java
@@ -1,6 +1,9 @@
 package at.kraweu.collisionDetection;
 
+import javafx.scene.shape.Line;
+
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 /**
  * Created by Kraweu on 30.07.2015.
@@ -17,6 +20,11 @@ public class Border
     }
     public static boolean inside(Point coordinate,Point borderEnd, Point borderBeginn, int sizex, int sizey)
     {
-        return false;
+        if (coordinate.x<borderBeginn.x||coordinate.x+sizex>borderEnd.x)
+            return false;
+        if (coordinate.y<borderBeginn.y||coordinate.y+sizey>borderEnd.y)
+            return false;
+        return true;
     }
+
 }
--- core/src/at/kraweu/collisionDetection/Objects.java
+++ core/src/at/kraweu/collisionDetection/Objects.java
@@ -1,8 +1,14 @@
 package at.kraweu.collisionDetection;
 
+import java.awt.*;
+
 /**
  * Created by Kraweu on 30.07.2015.
  */
 public class Objects
 {
+    public static boolean inside(Point coordinateold,Point coordinatenew,Point borderEnd, Point borderBeginn, int sizex, int sizey)//TODO Only copied from Border
+    {
+        return false;
+    }
 }
