87aed7ea775935a7f4c7d516b40f5f25c684917f
Kraweu <kraweu@gmx.at>
2015-07-30 23:40:07 +0200

    Collison detection rename

---
+++ core/src/at/kraweu/collisionDetection/CollisionDetectionrename.java
@@ -0,0 +1,10 @@
+package at.kraweu.collisionDetection;
+
+/**
+ * Created by Kraweu on 30.07.2015.
+ */
+public class CollisionDetectionrename
+{
+    public Border border=new Border();
+    public Objects objects=new Objects();
+}
--- core/src/at/kraweu/collisionDetection/collisionDetection.java
+++
@@ -1,8 +0,0 @@
-package at.kraweu.collisionDetection;
-
-/**
- * Created by Kraweu on 30.07.2015.
- */
-public class collisionDetection
-{
-}
