a65ba4d4cc5de84399975c1b48683587450c9f39
Kraweu <kraweu@gmx.at>
2015-08-10 23:49:00 +0200

    Added ToDo

--- core/src/at/kraweu/starscroller/Weapon.java
+++ core/src/at/kraweu/starscroller/Weapon.java
@@ -1,54 +1,29 @@
 package at.kraweu.starscroller;
 
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
-
 /**
  * Created by Alex on 19.05.2015.
  */
 public class Weapon
 {
-    String name = "unnamed";
-    double reloadtime = 1;
     double lastshot = 0;
     double posx;    //Position relative to the ship
     double posy;
-    AtlasRegion asset=null;
-    double shotposx; //Position relative to the Weapon
-    double shotposy;
+    //todo Implement level and Damagemultiplier into damage calculation
+    int level = 0;
+    double damagemultiplier = 1;
+    WeaponType type = null;
 
-
-    //Projectile properties
-
-    double damage = 1;
-    double speedx;
-    double speedy;
-    int sizex;
-    int sizey;
-    double acceleration = 0;
-    double swaying = 0;
-    double rotation = 0;
-    AtlasRegion projectileasset=null;
-
-    public Weapon(String name, double reloadtime, double posx, double posy, AtlasRegion asset, double damage, double speedx, double speedy, int sizex, int sizey, AtlasRegion projectileasset)
+    public Weapon(WeaponType type, double posx, double posy)
     {
-        this.name = name;
-        this.reloadtime = reloadtime;
+        this.type = type;
         this.posx = posx;
         this.posy = posy;
-        this.asset = asset;
-        this.damage = damage;
-        this.speedx = speedx;
-        this.speedy = speedy;
-        this.sizex = sizex;
-        this.sizey = sizey;
-        this.projectileasset = projectileasset;
     }
 
     public Projectile shoot(double shipposx, double shipposy)
     {
         if (lastshot==0)
-            return new Projectile(damage,shipposx+posx+shotposx,shipposy+posy+shotposy,speedx,speedy,sizex,sizey,projectileasset);
+            return new Projectile(type.getDamage(), shipposx + posx + type.shotposx, shipposy + posy + type.shotposy, type.getSpeedx(), type.getSpeedy(), type.getSizex(), type.getSizey(), type.projectileasset);
         return null;
     }
     public void reload(float delta)
@@ -57,43 +32,5 @@ public class Weapon
             lastshot = lastshot-delta<0?0:lastshot-delta;
     }
 
-    public AtlasRegion getProjectileasset()
-    {
-        return projectileasset;
-    }
 
-    public void setProjectileasset(AtlasRegion projectileasset)
-    {
-        this.projectileasset = projectileasset;
-    }
-
-    public double getRotation()
-    {
-        return rotation;
-    }
-
-    public void setRotation(double rotation)
-    {
-        this.rotation = rotation;
-    }
-
-    public double getSwaying()
-    {
-        return swaying;
-    }
-
-    public void setSwaying(double swaying)
-    {
-        this.swaying = swaying;
-    }
-
-    public double getAcceleration()
-    {
-        return acceleration;
-    }
-
-    public void setAcceleration(double acceleration)
-    {
-        this.acceleration = acceleration;
-    }
 }
