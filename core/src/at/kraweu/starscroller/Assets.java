e0e8a8c4c233f895ddfe89efe874c10e7cdf109b
Kraweu <kraweu@gmx.at>
2015-08-10 23:19:45 +0200

    All Regions of pack file get Loaded and are accessible by String Name

--- core/src/at/kraweu/starscroller/Assets.java
+++ core/src/at/kraweu/starscroller/Assets.java
@@ -3,27 +3,60 @@ package at.kraweu.starscroller;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.Iterator;
 
 /**
  * Created by Alex on 19.05.2015.
  */
 public class Assets
 {
-    TextureAtlas assetatlas;
-    AtlasRegion ship;
-    AtlasRegion weapon;
-    AtlasRegion projectile;
+
+    private Array<AtlasRegion> regions;
+    private Iterator<AtlasRegion> iterator;
+    private String name;
 
     public Assets()
     {
-        assetatlas = new TextureAtlas("packedImgs/Starscroller.pack");
+        //Load all assets into regions
+        TextureAtlas assetatlas = new TextureAtlas("packedImgs/Starscroller.pack");
+
+        regions = assetatlas.getRegions();
 
 
+        iterator = regions.iterator();
+    }
 
-        ship = assetatlas.findRegion("playerShip1_green");
-        weapon = assetatlas.findRegion("weapon2");
-        projectile = assetatlas.findRegion("laserBlue01");
+    /**
+     * Returns AltasRegion of Specific Name
+     * Or Null if Not Found + Exception
+     */
+    public AtlasRegion getRegion(String name)
+    {
+        try
+        {
+            iterator = regions.iterator();
+            while (iterator.hasNext())
+            {
+                AtlasRegion region = iterator.next();
+                if ((region).name.equals(name))
+                {
+                    return region;
+                }
+            }
+            throw new RegionNotFound();
+        } catch (RegionNotFound regionNotFound)
+        {
+            System.out.println();
+            regionNotFound.printStackTrace();
+            System.out.println();
+            System.out.println("AtlasRegion not Found: " + name);
+        }
+        return null;
+    }
 
-
+    private static class RegionNotFound extends Throwable
+    {
     }
 }
